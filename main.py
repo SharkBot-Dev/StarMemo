2de9cc199a44192bf37f151815e43c89f305166e
Neko <modoggu12345@outlook.com>
2026-06-07 20:10:41 +0900

    fix: 少し修正

--- main.py
+++ main.py
@@ -201,4 +201,4 @@ def add_memo():
     return jsonify({"success": True})
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=5000, debug=True)
+    app.run("0.0.0.0", port=5000, debug=False)
