4015b1034100a6e352a178d7bb613036d20a6225
Neko <modoggu12345@outlook.com>
2026-06-07 21:24:32 +0900

    fix: ミス

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ def index():
         user = users_col.find_one({"code": request.cookies.get('code')})
         if not user:
             return redirect(url_for('login'))
+    user = users_col.find_one({"code": request.cookies.get('code')})
     return render_template("sky.html", username=user['username'], title=title, description=description)
 
 @app.get('/login')
